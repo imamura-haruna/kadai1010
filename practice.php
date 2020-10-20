985cfea21c3c7e995aa1a58cef633f82e22727e3
imamura-haruna <became.a.bubble.000@gmail.com>
2020-10-20 12:43:32 +0000

    PHP/Laravel 04 関数を理解しよう　の課題

--- practice.php
+++ practice.php
@@ -1,32 +1,63 @@
 <?php
-$name="haruna";
-if($name=="haruna"){
-    echo "私は $name です。";
-}else{
-    echo "あなたの名前ではありません。";
+
+//課題１
+function twice($result){
+    echo $result*2;
 }
 
-for($i=1;$i<10001;$i++){
-    echo $i;
+echo twice(2) . "\n";
+
+
+//課題２
+function f($a, $b){
+    $result=$a+$b;
+    echo $result;
 }
 
+echo f(4,3) . "\n";
 
-$fruits=array("apple","orenge","banana","lemon","peach");
 
-foreach($fruits as $fruit){
-    echo "$fruit";
-    echo "\n";
+//課題３
+function y($arr){
+    echo $arr[0]*$arr[1]*$arr[2]*$arr[3]*$arr[4];
 }
 
+echo y($arr=array(1, 3, 5 ,7, 9)) . "\n";
 
-$start = 1;
 
-$end = 100;
-++ $end ;
+//課題４
+ function max_array($arr){
+ $max_number = $arr[0];
+     foreach($arr as $a){
+         sort($arr);
+     }
 
-for($i = $start; $i < $end; $i++){
+ return $max_number;
+ }
 
-  if($i % 5 == 0){
-    echo $i;
-  }
-}
+
+//課題５
+//strip_tags
+$str="<h1>tech boost</h1>";
+echo strip_tags($str) . "\n";
+
+//array_push
+$lang=array("あ","い","う");
+array_push($lang,"え","お");
+print_r($lang);
+
+//array_merge
+$lang1=["か","き","く"];
+$lang2=["け","こ"];
+$lang3=array_merge($lang1,$lang2);
+print_r($lang3);
+
+//time, mktime
+echo time() . "\n";
+echo mktime() . "\n";
+
+//date
+date_default_timezone_set('Asia/Tokyo');
+echo date("Y/m/d H:i:s") . "\n";
+
+?>
